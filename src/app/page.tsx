b3259f6b29ba81f3cfec79a4940bb4c4f6e70caa
Bnaya <bnaya98@gmail.com>
2024-03-26 20:34:18 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-sm text-zinc-500 px-10">
         The pictures are there, you just need to take them.
         </h2>
       </div>
