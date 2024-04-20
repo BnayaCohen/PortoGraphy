e69133c42bcf13c3af39a1e1bc554e823954c75e
Bnaya <bnaya98@gmail.com>
2024-04-20 20:32:44 +0300

    fixees

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
-import withPlaiceholder from "@plaiceholder/next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
--- src/app/components/imgContainer.tsx
+++ src/app/components/imgContainer.tsx
@@ -1,9 +1,5 @@
-import { useEffect, useState } from "react";
 import Image from "next/image"
-import fs from "node:fs/promises";
-import { getPlaiceholder } from "plaiceholder";
-
-
+import { useState } from "react"
 
 type Props = {
   photoSrc: string
@@ -14,36 +10,15 @@ export default function ImgContainer({ photoSrc }: Props) {
   const widthHeightRatio = 375 / 250 // photo.height / photo.width
   const galleryHeight = Math.ceil(250 * widthHeightRatio)
   const photoSpans = Math.ceil(galleryHeight / 10) + 1
-  let [blurrredURL, setBlurredURL] = useState<string>('')
-
-  const getBase64 = async (imgPath: string) => {
-    try {
-      const file = await fs.readFile(imgPath)
-      const { base64 } = await getPlaiceholder(file)
-      console.log(base64);
-      return base64
-  
-    } catch (error: unknown) {
-      // //error handling
-      // if (error instanceof Error) return error.message
-      // else if (error && typeof error === "object" && "message" in error)
-      //   return error.message as string
-      // else if (typeof error === "string") return error;
-      // else return "Unexpected error!"
-    }
-  }
-
-  useEffect(() => {
-    console.log(blurrredURL);
-    // getBase64(photoSrc).then(setBlurredURL)
-    console.log(blurrredURL);
-  }, []);
+  const [hovered, setHovered] = useState(false);
 
   return (
     <div className="w-full sm:w-[] md:w-[`calc(100vw / 3)`] mb-[2px] justify-self-center"
+    onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
       style={{ gridRow: `span ${photoSpans}` }}>
       <div className="grid place-content-center">
-        <div className="rounded-[1px] overflow-hidden group">
+        <div className="relative rounded-[1px] overflow-hidden group">
           <Image
             src={photoSrc}
             alt={"photo"}
@@ -57,9 +32,15 @@ export default function ImgContainer({ photoSrc }: Props) {
             // sizes="250px"
             sizes="100vw"
             placeholder="blur"
-            blurDataURL={"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPcuXdvPQAHHgK0ao3GhAAAAABJRU5ErkJggg=="}
-            className="group-hover:opacity-65 cursor-pointer"
+            blurDataURL={"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mPcXg8AAfMBOOg8T0oAAAAASUVORK5CYII="}
+            className="group-hover:opacity-65 cursor-pointer group-hover:hidden-text:opacity-100"
+            
           />
+          {hovered && (
+            <div className="absolute inset-0 flex items-center justify-center z-50">
+              <div className="text-zinc-600 font-semibold px-4 py-2 rounded-md">View</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
