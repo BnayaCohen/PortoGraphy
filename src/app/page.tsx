1bc420252e61a766495553fb94831ebe5fca81f4
Bnaya <bnaya98@gmail.com>
2024-04-22 15:48:06 +0300

    fixees

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,20 +30,9 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={400}
       />
-      <h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+      <h1 className="z-10 text-4xl pb-2 text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
         Bnaya Cohen
       </h1>
-
-
-      {/* <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        className="dark:invert"
-        width={100}
-        height={24}
-        priority
-      /> */}
-
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
